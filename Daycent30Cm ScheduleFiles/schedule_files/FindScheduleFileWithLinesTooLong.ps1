c386b83d37778467e57e7851236b1c2942734d8a
newneonova <botline@gmail.com>
2022-07-21 16:06:53 -0600

    Wrote powershell to split up schedule file lines that are too long into two lines and make new copies of the modified files

---
+++ Daycent30Cm ScheduleFiles/schedule_files/FindScheduleFileWithLinesTooLong.ps1
@@ -0,0 +1,40 @@
+$eqruns = ".\spinup\"
+$outdir = ".\fixedSpinups\"
+$count=0
+(Get-ChildItem -Path $eqruns -Filter '*.sch' -File) | ForEach-Object {
+   
+    $file = $_.FullName
+    $do=$false
+     $result = switch -File $file {
+     { $_.Length -ge 92 } {
+     $do=$true
+     break
+     }
+     }
+     $newlines=@()
+     if($do){
+     $result = switch -File $file {
+     { $_.Length -ge 92 } {
+      
+        $subline = $_.subString(0,89)
+        $index=$subline.LastIndexOf(',')+1
+        $line1 = $_.subString(0,$index)
+        $line2 = $_.subString($index)
+        $newlines+=$line1
+        $newlines+=$line2
+      
+        } 
+        default { $newlines+=$_}    
+
+     }
+     $outname = $outdir+((Split-Path $file -leaf).Trim())
+      Write-Host $outname
+            $newlines | Out-File -Append  $outname
+    }
+    
+ 
+    
+
+  
+}
+  Write-Host $count
