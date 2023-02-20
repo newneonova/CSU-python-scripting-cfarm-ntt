a9b968147a1c350288dc20c375fb8eda2e26c2fb
newneonova <botline@gmail.com>
2023-02-20 08:56:23 -0700

    Some work on finding missing weather data in 20cm daycent service
    Count not complete work because we're missing the original weather files so cant find any files that exist but arent in the service

--- (2018)MissingWeatherFileFromRunningCode/CompareSiteFileArchiveWithKnownFiles/Program.cs
+++ (2018)MissingWeatherFileFromRunningCode/CompareSiteFileArchiveWithKnownFiles/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Collections.Concurrent;
 using System.ComponentModel.DataAnnotations;
+using System.Xml.XPath;
 
 Console.WriteLine("Hello, World!");
 
@@ -10,48 +11,55 @@ Console.WriteLine("Hello, World!");
 var LargeFileLocaton = "D:\\LargeFileRepos\\2018daycent\\DaycentService\\JavaVersionDaycentService\\BigFilesDontCopy\\eqruns2017_spinup_20200205.csv";
 
 
-var AllBags = new ConcurrentBag<HashSet<string>>(); 
+var allCodes = new ConcurrentBag<(string,string)>();
 
-var AllLines = File.ReadAllLinesAsync(LargeFileLocaton).Result.Chunk(10000);
+
 
 
 //OK 700,000 lines, maybe do an ind
-           Parallel.ForEach(AllLines, (s,stat,idex) =>
+
+//OK this is very big and very slow so going to do it in bits
+           Parallel.ForEach( File.ReadAllLines(LargeFileLocaton), (s,stat,idex) =>
             {
-                var bigList = new List<string>();
-                foreach(var line in s)
-                {
-                      var asplit = line.Split(',');
-                int x = 0;
-                int y = 0;
-                int.TryParse(asplit[3], out x);
-                int.TryParse(asplit[4],out y);
-                int lower = -Radius;
+                   var asplit = s.Split(',');
+                allCodes.Add((asplit[3], asplit[4]));
+         
+            });
+var allCodesHash = allCodes.ToHashSet();
+var allCodeInt = allCodesHash.Select(pair =>
+{
+    int x = 0;
+    int y = 0;
+    int.TryParse(pair.Item1, out x);
+    int.TryParse(pair.Item2, out y);
+    return (x, y);
+}).ToList();
+
+
+var allAllALLLCODES = new ConcurrentDictionary<string, byte>();
+Parallel.ForEach(allCodeInt, pair =>
+{
+    var x= pair.Item1;
+    var y= pair.Item2;
+    int lower = -Radius;
                 int upper = Radius;
                 for(var X=lower; X <= upper; X++)
                 {
                     for(var Y=lower; Y<=upper; Y++)
                     {
-                         bigList.Add((y+Y) + "," +(x+X));
+                         allAllALLLCODES[(y+Y) + "," +(x+X)]=0;
                     }
                 }
 
-                }
-                AllBags.Add(bigList.ToHashSet());
-               
+                
 
-               
-         
-            });
+});
 
-var allKnownYXPairsList  = new List<string>();
-foreach(var bag in AllBags)
-{
-    allKnownYXPairsList.AddRange(bag.ToList());
-}
-var allKnownYXPairs= allKnownYXPairsList.ToHashSet();
 
-var allHadFiles = File.ReadAllLines("\"C:\\Users\\mclay\\Documents\\Github\\CSU-python-scripting-cfarm-ntt\\(2018)MissingWeatherFileFromRunningCode\\2018existingWeatherFiles.txt\"t").ToHashSet();
+
+var allKnownYXPairs= allAllALLLCODES.Keys;
+
+var allHadFiles = File.ReadAllLines("C:\\Users\\mclay\\Documents\\Github\\CSU-python-scripting-cfarm-ntt\\(2018)MissingWeatherFileFromRunningCode\\2018existingWeatherFiles.txt").ToHashSet();
 
 var inSiteArchiveButNotInFileSystem = allKnownYXPairs.Except(allHadFiles).ToList();
 var inFilesButNotInSiteArchive = allHadFiles.Except(allKnownYXPairs);
